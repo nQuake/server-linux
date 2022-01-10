f3c6ea4caba2a4ae29b4909f5e95f0048c0275f3
Andreas Beisler <eb@subdevice.org>
2022-01-10 14:57:34 +0100

    Add cmdline options to the start script. --regenerate: Regenerates all the configurations --generate-only: Doesn't start the server

--- scripts/start_servers.sh
+++ scripts/start_servers.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+REGENERATE=""
+GENERATE_ONLY=""
+
+for i in "$@"; do
+  case ${i} in
+    --regenerate)
+      REGENERATE=1
+      shift
+      ;;
+    --generate-only)
+      GENERATE_ONLY=1
+      shift
+      ;;
+  esac
+done
+
 installdir=$(cat ~/.nquakesv/install_dir)
 
 . ~/.nquakesv/config
@@ -8,7 +24,7 @@ generate_server_config() {
   inputfile=$1
   outputfile=${installdir}/ktx/pwd.cfg
 
-  [ ! -f ${outputfile} ] && {
+  ( [ ! -f ${outputfile} ] || [ ! -z ${REGENERATE} ] ) && {
     echo "rcon_password \"${SV_RCON}\"" > ${inputfile}
     echo "qtv_password \"\"" >> ${inputfile}
     [ "$(readlink -f $inputfile)" != "$(readlink -f $outputfile)" ] && cp -fn ${inputfile} ${outputfile}
@@ -20,7 +36,7 @@ generate_qtv_config() {
   inputfile=$2
   outputfile=$3
 
-  [ ! -f ${outputfile} ] && {
+  ( [ ! -f ${outputfile} ] || [ ! -z ${REGENERATE} ] ) && {
     [ "$(readlink -f $inputfile)" != "$(readlink -f $outputfile)" ] && cp -r ${inputfile} ${outputfile}
     echo "hostname \"${SV_HOSTNAME} Qtv\"" >> ${outputfile}
     echo "admin_password \"${SV_QTVPASS}\"" >> ${outputfile}
@@ -39,7 +55,7 @@ generate_qtv_config() {
 generate_qtv_script() {
   outputfile=${installdir}/run/qtv.sh
 
-  [ ! -f ${outputfile} ] && {
+  ( [ ! -f ${outputfile} ] || [ ! -z ${REGENERATE} ] ) && {
     echo "cd \$(cat ~/.nquakesv/install_dir)/qtv/ && screen -dmS qtv ./qtv.bin +exec qtv.cfg" > ${outputfile}
     chmod +x ${outputfile}
   }
@@ -50,7 +66,7 @@ generate_qwfwd_config() {
   inputfile=$2
   outputfile=$3
 
-  [ ! -f ${outputfile} ] && {
+  ( [ ! -f ${outputfile} ] || [ ! -z ${REGENERATE} ] ) && {
     [ "$(readlink -f $inputfile)" != "$(readlink -f $outputfile)" ] && cp -r ${inputfile} ${outputfile}
     echo "set hostname \"${SV_HOSTNAME} QWfwd\"" >> ${outputfile}
     echo "set net_port ${port}" >> ${outputfile}
@@ -60,7 +76,7 @@ generate_qwfwd_config() {
 generate_qwfwd_script() {
   outputfile=${installdir}/run/qwfwd.sh
 
-  [ ! -f ${outputfile} ] && {
+  ( [ ! -f ${outputfile} ] || [ ! -z ${REGENERATE} ] ) && {
     echo "cd \$(cat ~/.nquakesv/install_dir)/qwfwd/ && screen -dmS qwfwd ./qwfwd.bin" > ${outputfile}
     chmod +x ${outputfile}
   }
@@ -71,7 +87,7 @@ generate_port_config() {
   num=$2
   outputfile=$3
 
-  [ ! -f ${outputfile} ] && {
+  ( [ ! -f ${outputfile} ] || [ ! -z ${REGENERATE} ] ) && {
     cat ${installdir}/ktx/port_template.cfg > ${outputfile}
     echo "set k_motd1 \"${SV_HOSTNAME} #${num}\"" >> ${outputfile}
     echo "hostname \"${SV_HOSTNAME}:${port}\"" >> ${outputfile}
@@ -86,7 +102,7 @@ generate_port_script() {
   num=$2
   outputfile=$3
 
-  [ ! -f ${outputfile} ] && {
+  ( [ ! -f ${outputfile} ] || [ ! -z ${REGENERATE} ] ) && {
     echo "cd \$(cat ~/.nquakesv/install_dir)/ && screen -dmS qw_$port ./mvdsv -port ${port} -game ktx +exec port_${port}.cfg" > ${outputfile}
     chmod +x ${outputfile}
   }
@@ -99,35 +115,47 @@ for f in ~/.nquakesv/ports/*; do
   num=$((${num}+1))
   port=$(basename ${f})
   count=$(ps ax | grep -v grep | grep "mvdsv -port ${port}" | wc -l)
-  printf "* Starting mvdsv #${num} (port ${port})..."
   [ ${count} -eq 0 ] && {
     generate_port_config ${port} ${num} ${installdir}/ktx/port_${port}.cfg
     generate_port_script ${port} ${num} ${installdir}/run/port_${port}.sh
-    ${installdir}/run/port_${port}.sh > /dev/null &
-    echo "[OK]"
-  } || echo "[ALREADY RUNNING]"
+  }
+  [ -z "${GENERATE_ONLY}" ] && {
+    printf "* Starting mvdsv #${num} (port ${port})..."
+    [ ${count} -eq 0 ] && {
+      ${installdir}/run/port_${port}.sh > /dev/null &
+      echo "[OK]"
+    } || echo "[ALREADY RUNNING]"
+  }
 done
 
 [ -f ~/.nquakesv/qtv ] && {
   qtvport=$(cat ~/.nquakesv/qtv)
-  generate_qtv_config ${qtvport} ${installdir}/qtv/qtv_template.cfg ${installdir}/qtv/qtv.cfg
-  printf "* Starting qtv (port ${qtvport})..."
   count=$(ps ax | grep -v grep | grep "qtv.bin +exec qtv.cfg" | wc -l)
   [ ${count} -eq 0 ] && {
+    generate_qtv_config ${qtvport} ${installdir}/qtv/qtv_template.cfg ${installdir}/qtv/qtv.cfg
     generate_qtv_script
-    $(cat ~/.nquakesv/install_dir)/run/qtv.sh > /dev/null &
-    echo "[OK]"
-  } || echo "[ALREADY RUNNING]"
+  }
+  [ -z "${GENERATE_ONLY}" ] && {
+    printf "* Starting qtv (port ${qtvport})..."
+    [ ${count} -eq 0 ] && {
+      $(cat ~/.nquakesv/install_dir)/run/qtv.sh > /dev/null &
+      echo "[OK]"
+    } || echo "[ALREADY RUNNING]"
+  }
 }
 
 [ -f ~/.nquakesv/qwfwd ] && {
   qwfwdport=$(cat ~/.nquakesv/qwfwd)
-  generate_qwfwd_config ${qwfwdport} ${installdir}/qwfwd/qwfwd_template.cfg ${installdir}/qwfwd/qwfwd.cfg
-  printf "* Starting qwfwd (port ${qwfwdport})..."
   count=$(ps ax | grep -v grep | grep "./qwfwd.bin" | wc -l)
   [ ${count} -eq 0 ] && {
+    generate_qwfwd_config ${qwfwdport} ${installdir}/qwfwd/qwfwd_template.cfg ${installdir}/qwfwd/qwfwd.cfg
     generate_qwfwd_script
-    $(cat ~/.nquakesv/install_dir)/run/qwfwd.sh > /dev/null &
-    echo "[OK]"
-  } || echo "[ALREADY RUNNING]"
+  }
+  [ -z "${GENERATE_ONLY}" ] && {
+    printf "* Starting qwfwd (port ${qwfwdport})..."
+    [ ${count} -eq 0 ] && {
+      $(cat ~/.nquakesv/install_dir)/run/qwfwd.sh > /dev/null &
+      echo "[OK]"
+    } || echo "[ALREADY RUNNING]"
+  }
 }
